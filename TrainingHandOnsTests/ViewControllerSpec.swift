a3616d03d4cd4670e926f8ef811b5a6e3a9feed6
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2019-04-11 16:36:48 -0300

    Add all required frameworks for testing

---
+++ TrainingHandOnsTests/ViewControllerSpec.swift
@@ -0,0 +1,20 @@
+//
+//  ViewControllerSpec.swift
+//  TrainingHandOnsTests
+//
+//  Created by r.da.silva.ferreira on 4/11/19.
+//  Copyright © 2019 Concrete. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import TrainingHandOns
+
+final class ViewControllerSpec: QuickSpec {
+    override func spec() {
+        describe("a view controller") {
+
+        }
+    }
+}
